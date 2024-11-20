d7e214c1733a2b46d2a3046ecd5e0b73e8d3282e
chunghoang123 <chungss7890@gmail.com>
2024-11-20 21:49:51 +0700

    Add files via upload

---
+++ cau2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int firstNumber = 10;
+	int secondNumber = 5;
+	int tong = firstNumber+secondNumber;
+	printf("tong la %d\n", tong);
+	
+	return 0;
+}
---
+++ cau5.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int dai = 5;
+	int rong = 3;
+	int chuVi = 2*(dai+rong);
+	int dienTich = dai*rong;
+	printf(" chu vi hinh chu nhat la %d\n", chuVi);
+	printf(" dien tich hinh chu nhat la %d\n",dienTich);
+	
+	
+	return 0;
+}
